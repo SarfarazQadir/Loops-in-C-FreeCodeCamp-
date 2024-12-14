350f9fd7986ca2e546d8d77486a17dee58494d4b
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-12-14 10:13:30 +0500

    Add project files.

---
+++ Loops in C# (FreeCodeCamp)/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("For Loop");
+
+//      FizzBuzz Challenge
+
+for (int i = 1; i < 101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i}-FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i}-Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i}-Buzz");
+    else
+       Console.WriteLine(i);
+    
+
+}
+
+
+
+//  !st way
+//string[] names = { "Alex", "Eddie", "David", "Michael" };
+//for (int i = 0; i < names.Length; i++)
+//    if (names[i] == "David") names[i] = "Sammy";
+
+
+//foreach (var name in names) Console.WriteLine("foreach======>>>>>>>"+name);
+
+//  2nd way
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David")
+    {
+        names[i] = "Sammy";
+    }
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+
+
